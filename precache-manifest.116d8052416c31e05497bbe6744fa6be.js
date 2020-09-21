b7ff355f625c92245f844a9a9ddb7132ae21cd49
sebagl <sglonzieme@gmail.com>
2020-09-21 12:17:45 -0500

    Updates

---
+++ precache-manifest.116d8052416c31e05497bbe6744fa6be.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c4eac22e476767f972e767e72fb1b2c",
+    "url": "/stream-readers/index.html"
+  },
+  {
+    "revision": "bb91e0ac3c61c7142dad",
+    "url": "/stream-readers/static/css/main.d166a76b.chunk.css"
+  },
+  {
+    "revision": "732192e8c9de0f814f49",
+    "url": "/stream-readers/static/js/2.ad14f063.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/stream-readers/static/js/2.ad14f063.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb91e0ac3c61c7142dad",
+    "url": "/stream-readers/static/js/main.bc9bbdd2.chunk.js"
+  },
+  {
+    "revision": "e280d9d573ebf8e97252",
+    "url": "/stream-readers/static/js/runtime-main.90d3f407.js"
+  },
+  {
+    "revision": "501110e6edc0613fe7953bcc3edcd4ce",
+    "url": "/stream-readers/static/media/Foto pag filtro blanca.501110e6.png"
+  },
+  {
+    "revision": "c98e90dbeea13ceb85b00f91c188b2c9",
+    "url": "/stream-readers/static/media/Marian Sabado-03.c98e90db.jpg"
+  },
+  {
+    "revision": "8b6c811f0797b7fdf40aeb2485ac430e",
+    "url": "/stream-readers/static/media/TAPA PRINCIPITO ALTA-08 (1).8b6c811f.jpg"
+  },
+  {
+    "revision": "fa3f8679652b4517d15b77d835c0ea2e",
+    "url": "/stream-readers/static/media/WEB STREAM READERS Muestra-02 (1).fa3f8679.jpg"
+  },
+  {
+    "revision": "b3c74c9843e7e840993f4f865699f3ef",
+    "url": "/stream-readers/static/media/autopista_del_sur_nuevo2.b3c74c98.jpg"
+  },
+  {
+    "revision": "5e6a7b434abb8670944b62f15e794dd1",
+    "url": "/stream-readers/static/media/axolotl_nuevo2.5e6a7b43.jpg"
+  },
+  {
+    "revision": "2796ea9239e2b5ab7daeac5706c817e6",
+    "url": "/stream-readers/static/media/el-arte.2796ea92.jpg"
+  },
+  {
+    "revision": "a560e6a0ba5cdfef5db088c0f5cf0d08",
+    "url": "/stream-readers/static/media/el-kybalion.a560e6a0.jpg"
+  },
+  {
+    "revision": "220826596660edc8774a1632fbe7f28f",
+    "url": "/stream-readers/static/media/el-lobo-tapa.22082659.jpg"
+  },
+  {
+    "revision": "db28761d3915eca32f42160c9a30b1a5",
+    "url": "/stream-readers/static/media/el-lobo.db28761d.jpg"
+  },
+  {
+    "revision": "2cef87dce17d4022ba8f7d9b7a847892",
+    "url": "/stream-readers/static/media/el-principito.2cef87dc.jpg"
+  },
+  {
+    "revision": "4eec58e5ff1fd7faa1ec4cd9c0afab87",
+    "url": "/stream-readers/static/media/el_anticristo.4eec58e5.jpg"
+  },
+  {
+    "revision": "e12e6d8f367c3c893d393fb262d85c5e",
+    "url": "/stream-readers/static/media/el_corazon_delator.e12e6d8f.jpg"
+  },
+  {
+    "revision": "8216790468b780e919f93267a164db55",
+    "url": "/stream-readers/static/media/el_cuervo.82167904.jpg"
+  },
+  {
+    "revision": "9c188da8a095bf13ce565a56ec4b26f5",
+    "url": "/stream-readers/static/media/gato-negro.9c188da8.jpg"
+  },
+  {
+    "revision": "888295aaff41ae8024d91bc303b56bba",
+    "url": "/stream-readers/static/media/henry-be-228194-unsplash.888295aa.jpg"
+  },
+  {
+    "revision": "3b0f1a140f0d16e8ef5349dd038debe2",
+    "url": "/stream-readers/static/media/la-caida.3b0f1a14.jpg"
+  },
+  {
+    "revision": "3aa1415f81d4ecfe1faba21439f43679",
+    "url": "/stream-readers/static/media/la-metamorfosis.3aa1415f.jpg"
+  },
+  {
+    "revision": "ec80de12a0b680401ee5068dd0c5e5ad",
+    "url": "/stream-readers/static/media/nosotros.ec80de12.jpg"
+  },
+  {
+    "revision": "c07a361877ee423dfad568b18890efca",
+    "url": "/stream-readers/static/media/porque.c07a3618.jpg"
+  },
+  {
+    "revision": "e3003f26a96619cb34b0ce1077fb0dda",
+    "url": "/stream-readers/static/media/ruinas_circulares_nuevo2.e3003f26.jpg"
+  },
+  {
+    "revision": "13b9b24cfe03a00283da6e87c5929c9e",
+    "url": "/stream-readers/static/media/senorita_cora_nuevo2.13b9b24c.jpg"
+  },
+  {
+    "revision": "b5086a7b92dc8284ff4ada72bcd2d09c",
+    "url": "/stream-readers/static/media/siddartha.b5086a7b.jpg"
+  },
+  {
+    "revision": "22bea7302e8b7583195a1b405a1d9e89",
+    "url": "/stream-readers/static/media/tapa-siddartha.22bea730.jpeg"
+  }
+]);
